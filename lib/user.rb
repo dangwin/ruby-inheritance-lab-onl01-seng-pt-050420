a8eaff4083dc1611c02a93ab10098731b5c02e39
Dang Nguyen <danghwin@gmail.com>
2020-06-03 23:23:46 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,7 +2,10 @@ def User
   
   attr_accessor :first_name, :last_name
   
-  
-  
-end 
+  def initialize(first_name = nil, last_name = nil)
+    @first_name = first_name
+    @last_name = last_name
+  end
+end
+ 
   
